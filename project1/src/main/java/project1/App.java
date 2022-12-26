bdd791d9c044982e985f09b6e3275b3b9ab54580
suresh <sureshpydi@gmail.com>
2022-12-26 12:28:23 +0530

    project1 update siva

--- project1/src/main/java/project1/App.java
+++ project1/src/main/java/project1/App.java
@@ -1,13 +1,13 @@
 package project1;
 
 /**
- * Hello world!
+ * Hello siva!
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello siva!" );
     }
 }
